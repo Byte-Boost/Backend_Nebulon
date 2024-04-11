fdc873a98aee92ce09fd8c908534eb49443fb07e
Vinicius Forcato <76211125+nininhosam@users.noreply.github.com>
2024-04-11 15:17:56 -0300

    [feat]: finished user register/login routes

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,40 +1,43 @@
 const { User } = require("../models");
-const service = require('../services/users.services.js')
+const service = require("../services/users.services.js");
 
 class requestHandler {
   // POST
   registerUser = async (req, res) => {
-    let { username, password } = req.body;
-    var user = {
-      username: username,
-      password: await service.hashes(password),
-    };
-    User.create(user).catch((err) => {
-      console.log(err);
-      res.status(400).send();
-    });
+    let { body } = req;
 
-    res.status(201).send();
+    var user = {
+      username: body.username,
+      password: await service.getHashed(body.password),
+    };
+
+    User.create(user)
+      .then(() => {
+        res.status(201).send();
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).send();
+      });
   };
   loginUser = async (req, res) => {
     let { body } = req;
-    const user = User.findOne({ where: { username: body.username } })
-    if (user){
-        const validatePass = await service.compareHash(body.password, user.password)
-        if (validatePass) {
-            const token = service.getToken(user);
-            res.status(200).json({ "token": token });
-        } else {
-            res.status(401).send({"error": "Incorrect password or user"});
-        }
-    } else {
-        res.status(404).send({"error": "User does not exist"});
-    }
-  };
 
+    const user = await User.findOne({ where: { username: body.username } });
+    
+    try {
+      if (!user) throw new Error("User does not exist");
+      const token = await service.login(user, body.password);
+      res.status(200).json({ token: token });
+    } catch (err) {
+      res.status(401).send({error:err.message});
+    }
+
+  };
   // DELETE
   deleteUser = (req, res) => {
     let { params } = req;
+
     User.destroy({ where: { id: params.id } })
       .then(res.status(200).send())
       .catch((err) => {
--- services/users.services.js
+++ services/users.services.js
@@ -1,21 +1,27 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 class userService {
-    hashes = async (password) => {
-        const bcrypt = require('bcrypt')
+
+    getHashed = async (password) => {
         const salt = await bcrypt.genSalt()  //create a random salt that goes before the password, like eUsFhiYouPassword
         const hashed = await bcrypt.hash(password, salt)  //Hash the password using the salt, is hashing like this password => eusfhiYouPassword
-        console.log("Hashed: " + hashed)
         return hashed
     }
     
     compareHash = async (password, hash) => {
-        const bcrypt = require('bcrypt')
-        console.log("Pass: " + password)
-        console.log("Hash: " + hash)
         return await bcrypt.compare(password,hash)
     }
 
     getToken = async (user) => {
-        return jwt.sign({ "id" : user.id,"email" : user.username}, process.env.SECRET);
+        const token = jwt.sign({ "id" : user.id, "username" : user.username}, process.env.SECRET);
+        return token;
+    }
+
+    login = async (user, password)=>{
+        const passwordMatches = await this.compareHash(password, user.password);
+        if(passwordMatches) return this.getToken(user);
+        else throw new Error("Invalid password or username");
     }
 }
 
