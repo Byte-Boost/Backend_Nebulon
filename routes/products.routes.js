7560eeeac9126102c44c59c9134db3b52bccf894
Vinicius Forcato <76211125+nininhosam@users.noreply.github.com>
2024-05-10 18:00:22 -0300

    [fix] removed forgotten code
    silly mistake.

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -4,7 +4,6 @@ const router = require('express').Router();
 router.post('/', controller.createProduct);
 router.get('/', controller.getProducts);
 router.get('/:id', controller.getProductById);
-router.get('/class/:class', controller.getProductsWithClass);
 router.put('/:id', controller.updateProduct);
 router.delete('/:id', controller.deleteProduct);
 
